ff6419e1b17205cd015c7e59c727d722fba7dd18
a516384 <a0309mcdnom@yahoo.co.jp>
2015-08-15 11:21:54 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ActiveRecord::Base
     # 名前は必須入力かつ20文字以内
     validates :name , length: {  maximum: 20 } , presence: true
+    # 年齢は数字入力かつ０歳以上
+    validates :age, numericality: true ,inclusion: { :in => 0..120 }, presence: true
+    validates :age, numericality: { only_integer: true }, presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
